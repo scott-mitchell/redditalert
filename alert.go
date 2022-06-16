670e5b7488562dde8eb42aab341be623ae59cd2f
Scott Mitchell <scott-mitchell@users.noreply.github.com>
2022-06-16 15:42:41 -0700

    initial commit

---
+++ alert.go
@@ -0,0 +1,179 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"regexp"
+	"strings"
+	"time"
+
+	"github.com/diamondburned/arikawa/api"
+	"github.com/diamondburned/arikawa/discord"
+	"github.com/diamondburned/arikawa/webhook"
+	"github.com/golang/glog"
+	"github.com/turnage/graw/reddit"
+)
+
+const (
+	eventTimeout        = time.Minute
+	redditOrange        = 0xFF4500
+	redditLogo          = "https://www.redditinc.com/assets/images/site/reddit-logo.png"
+	maxEmbedTitle       = 256
+	maxEmbedDescription = 2048
+)
+
+type filter struct {
+	name         string
+	subreddits   map[string]bool
+	textFilter   *regexp.Regexp
+	authorFilter *regexp.Regexp
+}
+
+type Alerter struct {
+	webhookID    discord.WebhookID
+	webhookToken string
+	filters      []filter
+}
+
+type redditEvent struct {
+	author         string
+	filterableText string
+	subreddit      string
+	createdUTC     uint64
+	body           string
+	title          string
+	permalink      string
+}
+
+func New(config *Config) (*Alerter, error) {
+	webhookID, err := discord.ParseSnowflake(config.WebhookID)
+	if err != nil {
+		return nil, fmt.Errorf("invalid webhook id %q: %v", config.WebhookID, err)
+	}
+
+	filters := make([]filter, 0, len(config.Filters))
+	for i, f := range config.Filters {
+		var textFilter, authorFilter *regexp.Regexp
+		var err error
+		if f.TextFilter != "" {
+			textFilter, err = regexp.Compile(f.TextFilter)
+			if err != nil {
+				return nil, fmt.Errorf("error regex parsing filter[%d].authorFilter(%q): %v", i, f.TextFilter, err)
+			}
+		}
+
+		if f.AuthorFilter != "" {
+			authorFilter, err = regexp.Compile(f.AuthorFilter)
+			if err != nil {
+				return nil, fmt.Errorf("error regex parsing filter[%d].authorFilter(%q): %v", i, f.AuthorFilter, err)
+			}
+		}
+
+		subreddits := make(map[string]bool, len(f.Subreddits))
+		for _, subreddit := range f.Subreddits {
+			subreddits[strings.ToLower(subreddit)] = true
+		}
+
+		filters = append(filters, filter{
+			name:         f.Name,
+			subreddits:   subreddits,
+			textFilter:   textFilter,
+			authorFilter: authorFilter,
+		})
+	}
+
+	return &Alerter{
+		webhookID:    discord.WebhookID(webhookID),
+		webhookToken: config.WebhookToken,
+		filters:      filters,
+	}, nil
+}
+
+func (a *Alerter) Post(post *reddit.Post) error {
+	glog.V(2).Infof("Post in r/%s by u/%s: %s", post.Subreddit, post.Author, post.Title)
+	return a.handleEvent(context.TODO(), redditEvent{
+		author:         post.Author,
+		filterableText: post.Title + post.SelfText,
+		subreddit:      post.Subreddit,
+		createdUTC:     post.CreatedUTC,
+		body:           post.SelfText,
+		title:          post.Title,
+		permalink:      post.Permalink,
+	})
+}
+
+func (a *Alerter) Comment(comment *reddit.Comment) error {
+	glog.V(2).Infof("Comment in r/%s by u/%s: %s", comment.Subreddit, comment.Author, comment.Body)
+	return a.handleEvent(context.TODO(), redditEvent{
+		author:         comment.Author,
+		filterableText: comment.Body,
+		subreddit:      comment.Subreddit,
+		createdUTC:     comment.CreatedUTC,
+		body:           comment.Body,
+		title:          comment.LinkTitle,
+		permalink:      comment.Permalink,
+	})
+}
+
+func truncate(s string, length int) string {
+	runes := []rune(s)
+	if len(runes) <= length {
+		return s
+	}
+	return string(runes[:length])
+}
+func (a *Alerter) handleEvent(ctx context.Context, event redditEvent) error {
+	ctx, cancel := context.WithTimeout(ctx, eventTimeout)
+	defer cancel()
+
+	matchingFilter := ""
+	for _, f := range a.filters {
+		if !f.subreddits[strings.ToLower(event.subreddit)] {
+			continue
+		}
+		if f.textFilter != nil && !f.textFilter.MatchString(event.filterableText) {
+			continue
+		}
+		if f.authorFilter != nil && !f.authorFilter.MatchString(event.author) {
+			continue
+		}
+		matchingFilter = f.name
+	}
+	if matchingFilter == "" {
+		return nil
+	}
+
+	url := fmt.Sprintf("https://reddit.com%s", event.permalink)
+	glog.Infof("Matched filter %q: %s", matchingFilter, url)
+
+	client := webhook.NewCustomClient(a.webhookID, a.webhookToken, webhook.DefaultHTTPClient.WithContext(ctx))
+	client.ExecuteAndWait(api.ExecuteWebhookData{
+		Embeds: []discord.Embed{
+			{
+				Title:       truncate(event.title, maxEmbedTitle),
+				Description: truncate(event.body, maxEmbedDescription),
+				URL:         url,
+				Color:       discord.Color(redditOrange),
+				Author: &discord.EmbedAuthor{
+					Name: event.author,
+					URL:  fmt.Sprintf("https://reddit.com/u/%s", event.author),
+				},
+				Timestamp: discord.NewTimestamp(time.Unix(int64(event.createdUTC), 0)),
+				Thumbnail: &discord.EmbedThumbnail{
+					URL: redditLogo,
+				},
+				Fields: []discord.EmbedField{
+					{
+						Name:  "Filter",
+						Value: matchingFilter,
+					},
+					{
+						Name:  "Link",
+						Value: url,
+					},
+				},
+			},
+		},
+	})
+	return nil
+}
---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/golang/glog"
+	"github.com/turnage/graw"
+	"github.com/turnage/graw/reddit"
+)
+
+var (
+	configFile    = flag.String("config-file", "config.json", "The config file to load")
+	pollingPeriod = flag.Duration("polling-period", 5*time.Minute, "The period to poll for new submissions and comments")
+)
+
+func main() {
+	ctx := context.Background()
+	flag.Parse()
+	glog.Infof("Starting...")
+
+	cfg, err := LoadConfig(ctx, *configFile)
+	if err != nil {
+		glog.Fatalf("Failed to load config: %v", err)
+	}
+	glog.Infof("Using config %+v", cfg)
+
+	uniqueSubreddits := make(map[string]bool)
+	for _, filter := range cfg.Filters {
+		for _, subreddit := range filter.Subreddits {
+			uniqueSubreddits[strings.ToLower(subreddit)] = true
+		}
+	}
+	subreddits := make([]string, 0, len(uniqueSubreddits))
+	for subreddit := range uniqueSubreddits {
+		subreddits = append(subreddits, subreddit)
+	}
+	alerter, err := New(cfg)
+	if err != nil {
+		glog.Fatalf("Failed to create alerter: %v", err)
+	}
+	script, err := reddit.NewScript(cfg.RedditUserAgent, *pollingPeriod)
+	if err != nil {
+		glog.Fatalf("Failed to create Reddit script: %v", err)
+	}
+
+	glog.Infof("Starting scan...")
+	stop, wait, err := graw.Scan(alerter, script, graw.Config{
+		Subreddits:        subreddits,
+		SubredditComments: subreddits,
+		Logger:            log.Default(),
+	})
+	if err != nil {
+		glog.Fatalf("Failed to start Reddit scan: %v", err)
+	}
+	defer stop()
+	if err := wait(); err != nil {
+		glog.Fatalf("Reddit Scan failed: %v", err)
+	}
+
+}
