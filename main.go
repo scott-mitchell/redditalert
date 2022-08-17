e5bcfff93c53c444738f16b3a967c65108dc9ce3
Scott Mitchell <scott-mitchell@users.noreply.github.com>
2022-08-16 23:21:23 -0700

    Sleep on 429 oath2 errors before exiting. This app is deployed very simply and always restarts, which leads to flooding reddit with oauth requests even on 429s.

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"log"
+	"net/http"
 	"os"
 	"strings"
 	"time"
@@ -10,12 +12,14 @@ import (
 	"github.com/golang/glog"
 	"github.com/turnage/graw"
 	"github.com/turnage/graw/reddit"
+	"golang.org/x/oauth2"
 )
 
 var (
 	configFile    = flag.String("config-file", "config.json", "The config file to load")
 	agentFile     = flag.String("agent-file", "agent.yaml", "The agent file to load")
 	pollingPeriod = flag.Duration("polling-period", 5*time.Minute, "The period to poll for new submissions and comments")
+	delay429      = flag.Duration("delay-429", 5*time.Minute, "How long to wait on 429 oauth2 errors before exiting.")
 )
 
 func main() {
@@ -54,6 +58,13 @@ func main() {
 		SubredditComments: subreddits,
 		Logger:            log.New(os.Stderr, "[Graw]", log.LstdFlags),
 	})
+	// Too make deployment for me simpler, wait in the app on 429s before exiting to avoid flooding reddit.
+	var retErr *oauth2.RetrieveError
+	if errors.As(err, &retErr) && retErr.Response.StatusCode == http.StatusTooManyRequests {
+		glog.Errorf("Got 429 from oauth, waiting %v before exiting", *delay429)
+		time.Sleep(*delay429)
+		glog.Fatalf("Oauth error starting reddit scan: %v", err)
+	}
 	if err != nil {
 		glog.Fatalf("Failed to start Reddit scan: %v", err)
 	}
